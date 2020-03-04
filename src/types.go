0db4d15528a8d9ccaddc9a9006e9e1c0ba8eb88a
jmaster98 <jo-ash@procentrica.co.za>
2020-03-04 09:49:23 +0200

    changed update user struct

--- src/types.go
+++ src/types.go
@@ -13,7 +13,6 @@ type RegisterUser struct {
 type UpdateUser struct {
 	UserID   string `json:"id"`
 	Username string `json:"username"`
-	Password string `json:"password"`
 	Name     string `json:"name"`
 	Surname  string `json:"surname"`
 	Email    string `json:"email"`
